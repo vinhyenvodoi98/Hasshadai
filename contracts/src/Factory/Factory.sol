e4f720d73d80e8de3f06cc6a06190724bff2007d
DuyHungLe <duyhung2201@gmail.com>
2024-08-25 11:36:43 -0400

    feat: factory create2 deploy function

--- contracts/src/Factory/Factory.sol
+++ contracts/src/Factory/Factory.sol
@@ -1,20 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../SeedifyFund/SeedifyFundERC20.sol";
+import "../Launchpad/LaunchpadToken.sol";
 
-contract ImplementationContract {
-    bool public isInitialized; //initializer function that will be called once, during
-
-    function initializer() external {
-        require(!isInitialized);
-        isInitialized = true;
-    } // rest of the implementation functions go here
-}
-
-contract MinimalProxyFactory {
-    address[] public proxies;
+contract LaunchpadFactory {
+    address[] public allLaunchpads;
     address public implementationContract;
+    event LaunchpadDeployed(address proxy, string name, address projectOwner);
 
     constructor(address _implementation) {
         implementationContract = _implementation;
@@ -28,71 +20,26 @@ contract MinimalProxyFactory {
         uint256 _noOfTiers,
         address _projectOwner,
         address _tokenAddress,
-        uint256 _totalUsers,
         uint8 _phaseNo
     ) external returns (address) {
-        // convert the address to 20 bytes
         bytes20 implementationContractInBytes = bytes20(implementationContract);
-        //address to assign a cloned proxy
+
         address proxy;
-
-        // as stated earlier, the minimal proxy has this bytecode
-        // <3d602d80600a3d3981f3363d3d373d3d3d363d73><address of implementation contract><5af43d82803e903d91602b57fd5bf3>
-
-        // <3d602d80600a3d3981f3> == creation code which copies runtime code into memory and deploys it
-
-        // <363d3d373d3d3d363d73> <address of implementation contract> <5af43d82803e903d91602b57fd5bf3> == runtime code that makes a delegatecall to the implentation contract
-
         assembly {
-            /*
-            reads the 32 bytes of memory starting at the pointer stored in 0x40
-            In solidity, the 0x40 slot in memory is special: it contains the "free memory pointer"
-            which points to the end of the currently allocated memory.
-            */
             let clone := mload(0x40)
-            // store 32 bytes to memory starting at "clone"
             mstore(
                 clone,
                 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
             )
-
-            /*
-              |              20 bytes                |
-            0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000
-                                                      ^
-                                                      pointer
-            */
-            // store 32 bytes to memory starting at "clone" + 20 bytes
-            // 0x14 = 20
             mstore(add(clone, 0x14), implementationContractInBytes)
-
-            /*
-              |               20 bytes               |                 20 bytes              |
-            0x3d602d80600a3d3981f3363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe
-                                                                                              ^
-                                                                                              pointer
-            */
-            // store 32 bytes to memory starting at "clone" + 40 bytes
-            // 0x28 = 40
             mstore(
                 add(clone, 0x28),
                 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000
             )
-
-            /*
-            |                 20 bytes                  |          20 bytes          |           15 bytes          |
-            0x3d602d80600a3d3981f3363d3d373d3d3d363d73b<implementationContractInBytes>5af43d82803e903d91602b57fd5bf3 == 45 bytes in total
-            */
-
-            // create a new contract
-            // send 0 Ether
-            // code starts at the pointer stored in "clone"
-            // code size == 0x37 (55 bytes)
             proxy := create(0, clone, 0x37)
         }
 
-        // Call initialization
-        SeedifyLaunchpadERC20(proxy).initialize(
+        LaunchpadERC20(proxy).initialize(
             _name,
             _maxCap,
             _saleStart,
@@ -100,10 +47,59 @@ contract MinimalProxyFactory {
             _noOfTiers,
             _projectOwner,
             _tokenAddress,
-            _totalUsers,
+            0,
             _phaseNo
         );
-        proxies.push(proxy);
+        allLaunchpads.push(proxy);
+
+        emit LaunchpadDeployed(proxy, _name, _projectOwner);
         return proxy;
+    }
+
+    error Create2EmptyBytecode();
+
+    error Create2FailedDeployment();
+
+    function deployWithCreate2(
+        string memory _name,
+        uint256 _maxCap,
+        uint256 _saleStart,
+        uint256 _saleEnd,
+        uint256 _noOfTiers,
+        address _projectOwner,
+        address _tokenAddress,
+        uint8 _phaseNo
+    ) external payable returns (address launchpad) {
+        bytes memory bytecode = type(LaunchpadERC20).creationCode;
+        bytes32 salt = keccak256(
+            abi.encodePacked(
+                _name,
+                _maxCap,
+                _saleStart,
+                _saleEnd,
+                _noOfTiers,
+                _projectOwner,
+                _tokenAddress,
+                _phaseNo
+            )
+        );
+        assembly {
+            launchpad := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        LaunchpadERC20(launchpad).initialize(
+            _name,
+            _maxCap,
+            _saleStart,
+            _saleEnd,
+            _noOfTiers,
+            _projectOwner,
+            _tokenAddress,
+            0,
+            _phaseNo
+        );
+        allLaunchpads.push(launchpad);
+
+        emit LaunchpadDeployed(launchpad, _name, _projectOwner);
+        return launchpad;
     }
 }
